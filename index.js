60975f398afb3126697be30d44271715add7461e
Amariah <amariah.abish@gmail.com>
2024-03-14 12:32:27 +0300

    C AND I DOM NODES IN JS

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const element = document.createElement("div");
+const main = document.getElementById("main");
+element.textContent = "Amariah Kamau is the champion";
+main.append(element);
